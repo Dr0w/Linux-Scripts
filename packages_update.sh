af356fc8efb67b692a4bd6e3cf2a962a37af8621
Yevhenii B. <drow.public@gmail.com>
2024-08-03 13:10:11 -0400

    Some polishing

--- packages_update.sh
+++ packages_update.sh
@@ -12,10 +12,28 @@ detect_os() {
   fi
 }
 
+# Function to detect the user's shell
+detect_shell() {
+  SHELL_NAME=$(basename "$SHELL")
+  SHELL_PROFILE=""
+  case "$SHELL_NAME" in
+    bash)
+      SHELL_PROFILE=".bash_profile"
+      ;;
+    zsh)
+      SHELL_PROFILE=".zprofile"
+      ;;
+    *)
+      echo "Unsupported shell: $SHELL_NAME"
+      exit 1
+      ;;
+  esac
+}
+
 # Function to update packages and handle logging
 update_packages() {
   LOGFILE="$HOME/update.log"
-  
+
   case "$OS" in
     centos)
       if [ "$VER" == "7" ]; then
@@ -62,4 +80,5 @@ update_packages() {
 
 # Main script execution
 detect_os
+detect_shell
 update_packages
